693896bff02edfa2da4e7538a135ad926f3865d5
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-25 18:06:15 +0300

    Create colors that will be used in the app

---
+++ src/styles/colors.js
@@ -0,0 +1,13 @@
+export default {
+  primaryBlue: '#77ACF1',
+  primaryGreen: '#65C18C',
+  primaryPink: '#FF7BA9',
+  secondaryBlue: '#C0FEFC',
+  secondaryGreen: '#C1F4C5',
+  secondaryPink: '#FFBED8',
+  plainBlue: '#3EDBF0',
+  primaryText: '#000',
+  secondaryText: '#FFF',
+  primaryBackground: '#FFF',
+  secondaryBackground: '#222',
+};
