531218bc5eed35a2e4901dab62d65b8270ac7754
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-26 17:28:24 +0300

    Change space structure

--- src/styles/spaces.js
+++ src/styles/spaces.js
@@ -1,9 +1,19 @@
 export default {
-  micro: 3,
-  tiny: 5,
-  small: 7,
-  medium: 10,
-  large: 12,
-  big: 15,
-  huge: 20,
+  margin: {
+    tiny: 5,
+    small: 7,
+    medium: 10,
+    large: 12,
+    big: 15,
+    huge: 20,
+  },
+  padding: {
+    micro: 3,
+    tiny: 5,
+    small: 7,
+    medium: 10,
+    large: 15,
+    big: 20,
+    huge: 50,
+  },
 };
