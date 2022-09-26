5584d88812efa54a2968de460c756977c865cde3
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-26 19:23:35 +0300

    Create toast messages

---
+++ src/utilities/toastMessages.js
@@ -0,0 +1,25 @@
+import Toast from 'react-native-root-toast'
+
+import colors from '../styles/colors';
+
+export const errorMessage=(message)=>{
+  Toast.show(message, {
+    duration: Toast.durations.LONG,
+    position: Toast.positions.TOP,
+    shadow: false,
+    hideOnPress: true,
+    backgroundColor: colors.secondaryPink,
+    textColor: colors.primaryText
+  });
+}
+
+export const successfulMessage=(message)=>{
+  Toast.show(message, {
+    duration: Toast.durations.LONG,
+    position: Toast.positions.TOP,
+    shadow: false,
+    hideOnPress: true,
+    backgroundColor: colors.secondaryGreen,
+    textColor: colors.primaryText
+  });
+}
