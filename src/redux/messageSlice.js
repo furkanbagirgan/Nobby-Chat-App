f20a8452feaa63f20b4be2689a14f16d01a78cf2
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-26 18:24:32 +0300

    Create message slice for message states

---
+++ src/redux/messageSlice.js
@@ -0,0 +1,36 @@
+import {createSlice} from '@reduxjs/toolkit';
+import {query, doc, where, onSnapshot} from 'firebase/firestore';
+
+//This is the slice where contact operations are made.
+const messageSlice = createSlice({
+  name: 'message',
+  initialState: {
+    messages: [],
+    cancelSnapshot: () => {},
+    loading: false,
+    error: false,
+  },
+  reducers: {
+    getMessages: (state, action) => {
+      try {
+        const data = [];
+        const chat = doc(db, 'chats', action.payload);
+        const unsubscribe = onSnapshot(chat, doc => {
+          data=[...doc.data().messages];
+        });
+        return {
+          cancelSnapshot: unsubscribe,
+          messages: data,
+        };
+      } catch {
+        return {
+          cancelSnapshot: () => {},
+          messages: [],
+        };
+      }
+    },
+  },
+});
+
+export const {getMessages} = messageSlice.actions;
+export default messageSlice.reducer;
