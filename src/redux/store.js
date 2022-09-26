13be6702c0af621146e9f6b94ed952b56d7ba177
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-26 17:28:43 +0300

    Add contact and chat slice to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,16 @@ import {configureStore} from '@reduxjs/toolkit';
 
 import AuthReducer from './authSlice';
 import ThemeReducer from './themeSlice';
+import ContactReducer from './contactSlice';
+import ChatReducer from './chatSlice';
 
 //A store is created by pulling reducers from all slices.
 const store = configureStore({
   reducer: {
     auth: AuthReducer,
     theme: ThemeReducer,
+    contact: ContactReducer,
+    chat: ChatReducer
   },
 });
 
