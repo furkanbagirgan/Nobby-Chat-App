d4d4fe71e45981c1dac4055ff02e4818b7c533e7
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-27 19:08:12 +0300

    Add navigation as child and wrap with provider

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text } from 'react-native';
-import { RootSiblingParent } from 'react-native-root-siblings';
+import React from 'react';
+import {RootSiblingParent} from 'react-native-root-siblings';
+import {Provider} from 'react-redux';
 
-export default function App() {
+import Navigation from './src/navigation';
+import store from './src/redux/store';
+
+const App = () => {
   return (
     <RootSiblingParent>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Provider store={store}>
+        <Navigation />
+      </Provider>
     </RootSiblingParent>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
