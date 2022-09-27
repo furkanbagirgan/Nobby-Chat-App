c5dd8e638be15fd40fc537aabc0ee9fcb4d6369d
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-27 18:54:06 +0300

    Create login screen

---
+++ src/screens/Login/Login.js
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+import styles from './Login.style';
+
+const Login = () => {
+  return (
+    <View style={styles[theme].container}>
+      <Text>Login</Text>
+    </View>
+  );
+};
+
+export default Login;
