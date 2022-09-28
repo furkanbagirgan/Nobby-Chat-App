9a3b38a59ce8fab49e8833eb019ce8e5d8ce0df2
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-28 17:12:35 +0300

    Add bottom tab and chat screen

--- src/navigation/ContentStack.js
+++ src/navigation/ContentStack.js
@@ -1,8 +1,11 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
+import {StyleSheet} from 'react-native';
 import {useSelector} from 'react-redux';
 
-import Home from '../screens/Home';
+import BottomTabs from './BottomTabs';
+import Chat from '../screens/Chat';
+import colors from '../styles/colors';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,14 +17,46 @@ const ContentStack = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen
-        name="Home"
-        component={Home}
-        options={{
-          headerShown: false,
-        }}
+        name="BottomTabs"
+        component={BottomTabs}
+        options={{headerShown: false}}
+      />
+      {/*On the right side of the chat screen, there is a picture of the person to message.*/}
+      <Stack.Screen
+        name="Chat"
+        component={Chat}
+        options={({route}) => ({
+          headerStyle: {
+            backgroundColor:
+              theme === 'light'
+                ? colors.lightBackground
+                : colors.darkBackground,
+          },
+          headerTintColor:
+            theme === 'light' ? colors.secondaryText : colors.secondaryText,
+          headerTitleAlign: 'center',
+          headerTitle: route.params.chatName,
+          headerShadowVisible: false,
+          headerBackVisible: true,
+          headerRight: () => (
+            <Image
+              source={{uri: route.params.receiverPhoto}}
+              style={styles.image}
+            />
+          ),
+        })}
       />
     </Stack.Navigator>
   );
 };
+
+//Here is the required style for the image to be displayed in the header of the chat screen.
+const styles = StyleSheet.create({
+  image: {
+    width: 32,
+    height: 32,
+    borderRadius: 16,
+  },
+});
 
 export default ContentStack;
