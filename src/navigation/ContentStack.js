c813c8dcb79e0ccce83f0e9f94d327280cbd4577
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-28 15:27:15 +0300

    Add home screen

--- src/navigation/ContentStack.js
+++ src/navigation/ContentStack.js
@@ -2,6 +2,8 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import {useSelector} from 'react-redux';
 
+import Home from '../screens/Home';
+
 const Stack = createNativeStackNavigator();
 
 //It is the navigation structure that will be displayed when the login is still made.
@@ -9,7 +11,17 @@ const ContentStack = () => {
   //The theme information is accessed with the useSelector hook.
   const theme = useSelector(state => state.theme.theme);
 
-  return <Stack.Navigator></Stack.Navigator>;
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
 };
 
 export default ContentStack;
