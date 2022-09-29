43b1b3f38105dd2529c035effe93ce501917b69d
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-29 19:17:27 +0300

    Delete chat and message slices

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,8 +3,6 @@ import {configureStore} from '@reduxjs/toolkit';
 import AuthReducer from './authSlice';
 import ThemeReducer from './themeSlice';
 import ContactReducer from './contactSlice';
-import ChatReducer from './chatSlice';
-import MessageReducer from './messageSlice';
 
 //A store is created by pulling reducers from all slices.
 const store = configureStore({
@@ -12,8 +10,6 @@ const store = configureStore({
     auth: AuthReducer,
     theme: ThemeReducer,
     contact: ContactReducer,
-    chat: ChatReducer,
-    message: MessageReducer
   },
 });
 
