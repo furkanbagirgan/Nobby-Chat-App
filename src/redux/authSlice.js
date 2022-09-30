511cde635c97489f479b3098db19f37d3ab39fc3
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-30 12:30:28 +0300

    Add new state

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -5,22 +5,32 @@ const authSlice = createSlice({
   name: 'auth',
   initialState: {
     currentUser: {},
+    userStory: false
   },
   reducers: {
     //The new incoming data is assigned to the existing currentUser.
     setCurrentUser: (state, action) => {
       return {
         currentUser: action.payload,
+        userStory: state.userStory,
       };
     },
     //The value of currentUser is being flushed.
     resetUser: (state, action) => {
       return {
         currentUser: {},
+        userStory: state.userStory,
+      };
+    },
+    //The value of currentUser is being flushed.
+    setUserStory: (state, action) => {
+      return {
+        currentUser: state.currentUser,
+        userStory: action.payload,
       };
     },
   },
 });
 
-export const {setCurrentUser, resetUser} = authSlice.actions;
+export const {setCurrentUser, resetUser,setUserStory} = authSlice.actions;
 export default authSlice.reducer;
