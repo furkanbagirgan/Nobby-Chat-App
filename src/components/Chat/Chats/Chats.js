b02481762764b71e09e215987560e504a78f7c2e
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-10-01 19:24:29 +0300

    Create chats component

---
+++ src/components/Chat/Chats/Chats.js
@@ -0,0 +1,83 @@
+import {FlatList, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  collection,
+  query,
+  where,
+  onSnapshot,
+  orderBy,
+  Timestamp,
+} from 'firebase/firestore';
+
+import styles from './Chats.style';
+import ChatCard from './../ChatCard';
+import {auth, db} from '../../../utilities/firebase';
+
+const Chats = ({chatDetail}) => {
+  //Necessary states are created.
+  const [chats, setChats] = useState([]);
+
+  useEffect(() => {
+    //Chats retrieved from the Firestore and saved in the chats state.
+    const q = query(
+      collection(db, 'message'),
+      where('members', 'array-contains', auth.currentUser.uid),
+      orderBy('lastDate', 'desc'),
+    );
+    const unsubscribe = onSnapshot(q, snapshot => {
+      const userChats = [];
+      snapshot.forEach(doc => {
+        userChats.push({...doc.data(), id: doc.id});
+      });
+      setChats([...userChats]);
+    });
+
+    //The unsubscribe function is executed when the screen is closed.
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  //Here is the function where key assignments of the fields to repeat in the flat list are made.
+  const keyExtractor = item => {
+    return item.id;
+  };
+
+  //Here, there is a function that adjusts how the areas to be repeated in the
+  //flat list will appear on the screen. Also, a chatCard component is created for each chat.
+  const renderItem = ({item}) => {
+    let members = item.members;
+    const index = members.indexOf(auth.currentUser.uid);
+    members.splice(index, 1);
+    let messageCount = 0;
+    item.messages.forEach(message => {
+      if (!message.seen) {
+        messageCount++;
+      }
+    });
+    return (
+      <ChatCard
+        userId={members[0]}
+        lastMessage={item.messages[item.messages.length - 1]}
+        messageCount={messageCount}
+        handlePress={chatDetail}
+      />
+    );
+  };
+
+  //Elements that will appear on the screen are defined here
+  return (
+    <View style={styles.container}>
+      <FlatList
+        keyExtractor={keyExtractor}
+        data={chats}
+        renderItem={renderItem}
+        overScrollMode="never"
+        bounces={false}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  );
+};
+
+export default Chats;
---
+++ src/components/Chat/Chats/Chats.style.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+import spaces from '../../../styles/spaces';
+
+//Here the styles of the Chats component are created.
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '90%',
+    paddingTop: spaces.padding.micro,
+    paddingBottom: spaces.padding.big,
+  },
+});
+
+export default styles;
